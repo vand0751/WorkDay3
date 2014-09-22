1087129ac1f2b1eedec785cc887a595db78e25c1
idsc3101 <idsc3101@gmail.com>
2014-09-22 15:21:43 -0500

    finished

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -11,20 +11,34 @@ Public Class Form1
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
 
-        num1 = CDbl(st1)
-        num2 = CDbl(st2)
+        'checkpoint 1
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("Check 1: Please type the numbers")
+            Return 'terminate event handler' 
+        End If
+        
+        'clean area 
+            num1 = CDbl(st1)
+            num2 = CDbl(st2)
+
+        'checkpoint 2
+        If Not (num1 >= 0 And num2 >= 0) Then
+            MessageBox.Show("Check 2: Please type the non-negative numbers")
+        End If
 
         Dim result As String
 
-        If num1 > num2 Then
-            result = "The larger number is " & num1
-        ElseIf num2 > num1 Then
-            result = "The larger number is " & num2
-        Else
-            result = "They are equal"
-        End If
+            If num1 > num2 Then
+                result = "The larger number is " & num1
+            ElseIf num2 > num1 Then
+                result = "The larger number is " & num2
+            Else
+                result = "They are equal"
+            End If
 
-        txtResult.Text = result
+            txtResult.Text = result
+
     End Sub
+
 
 End Class
